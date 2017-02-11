c0afceeea9204595d16f5a6be0c254317aab4af7
David Kitchen <dkitchen@cloudflare.com>
2017-02-11 19:38:21 +0000

    Add dict

--- transform/dictionary.go
+++ transform/dictionary.go
@@ -5,6 +5,11 @@ import "errors"
 // Dictionary creates a map[string]interface{} from the given parameters by
 // walking the parameters and treating them as key-value pairs.  The number
 // of parameters must be even.
+//
+// Copyright The Hugo Authors
+// License Apache 2
+// https://github.com/spf13/hugo/blob/master/LICENSE.md
+// https://github.com/spf13/hugo/blob/master/tpl/template_funcs.go
 func Dictionary(values ...interface{}) (map[string]interface{}, error) {
 	if len(values)%2 != 0 {
 		return nil, errors.New("invalid dict call")
